dac81b9604b8cf95b75d79cc6d1fbcb7f8dbd17f
Maxime Verriest <maxime.verriest@quadra-informatique.fr>
2021-09-30 11:50:47 +0200

    authentification user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+     {
+         $this->passwordHasher = $passwordHasher;
+     }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+       
+        $user->setPassword($this->passwordHasher->hashPassword(
+                         $user,
+                        'admin'
+                    ));
+        $user->setEmail("admin@admin.fr");
+        
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
